d654d8a03334715b4adfd32e2ac7c4279410b63e
ANUSREE KJ <anusree.k.jinan123@gmail.com>
2025-04-07 21:47:24 +0530

    Create cycle_1____replacing_word.py
    write a program to replace a word by another word in a sentence

---
+++ cycle_1____replacing_word.py
@@ -0,0 +1,7 @@
+sen = input("Enter a sentence.\n")
+
+old_word = input("Enter the word to be replaced.\n")
+new_word = input("Enter the new word.\n")
+
+sen = sen.replace(old_word , new_word)
+print("Modified sentence : \n",sen)
