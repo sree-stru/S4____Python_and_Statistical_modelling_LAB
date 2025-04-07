fde50ffbcd122ff032587d2ce7c351b0b60ae363
ANUSREE KJ <anusree.k.jinan123@gmail.com>
2025-04-07 21:13:16 +0530

    Create cycle_1____largest_of_3_numbers.py
    write a program to find the largest of 3 numbers

---
+++ cycle_1____largest_of_3_numbers.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number\n"))
+num2 = int(input("Enter the second number\n"))
+num3 = int(input("Enter the third number\n"))
+
+if num1 >= num2  and  num1 >= num3 :
+     max = num1
+if num2 >= num1  and  num2 >= num3 :
+     max = num2 
+if num3 >= num1  and  num3 >= num2 :
+     max = num3
